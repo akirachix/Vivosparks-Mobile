301d199fea939396b0713591d2c8b19f1a9bab89
majidhawa <majidhawaah@gmail.com>
2024-09-24 11:23:03 +0300

    add files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,8 @@ android {
     namespace = "com.akirachix.investikatrial"
     compileSdk = 34
 
+
+
     defaultConfig {
         applicationId = "com.akirachix.investikatrial"
         minSdk = 24
@@ -26,34 +28,46 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
     kotlinOptions {
         jvmTarget = "1.8"
     }
-
-
-    buildFeatures{
-        viewBinding = true
+    buildFeatures {
+        viewBinding=true
     }
 }
 
 
-
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.androidx.core.ktx.v160)
+    implementation(libs.androidx.appcompat.v131)
+    implementation(libs.material.v140)
     implementation(libs.androidx.activity.ktx)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.retrofit)
-    implementation(libs.converter.gson)
+    implementation(libs.androidx.constraintlayout.v210)
+    implementation(libs.retrofit.v290)
+    implementation(libs.converter.gson.v290)
     implementation(libs.androidx.activity)
-    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit.v113)
+    androidTestImplementation(libs.androidx.espresso.core.v340)
+    implementation (libs.androidx.appcompat.v151)
+    implementation(libs.material.v161)
+    implementation (libs.material.v180)
     androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    implementation (libs.material.v140)
+    implementation (libs.androidx.constraintlayout)
+    implementation (libs.play.services.auth)
+    implementation (libs.androidx.core.v1100)
+
+
+
+
+
+
 }
